b1e9174bed8bbb133b240c580f8da81fd8030e6b
Krista Rutz <rutzkri000@gmail.com>
2020-03-10 14:29:37 -0700

    Adds a volume Calculator and a Cost Calculator, and sets up pathways between pages

---
+++ Parcels/Controllers/ParcelsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+using System.Collections.Generic;
+
+namespace Parcels.Controllers
+{
+  public class ParcelsController : Controller
+  {
+    // [HttpGet("/parcel")]
+    // public ActionResult Index()
+    // {
+    //   return View();
+    // }
+
+    [HttpGet("/parcel/new")]
+    public ActionResult CreateParcel()
+    {
+      return View();
+    }
+
+    [HttpPost("/parcel")]
+    public ActionResult Create(string height, string length, string width, string weight)
+    {
+      Parcel newParcel = new Parcel(int.Parse(height), int.Parse(length), int.Parse(width), int.Parse(weight));
+      return View("Index", newParcel);
+    }
+  }
+}
--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -4,6 +4,32 @@ namespace Parcels.Models
 {
   public class Parcel
   {
+    // private int _height { get; set; }
+    // private int _length { get; set; }
+    // private int _width { get; set; }
+    // private int _weight { get; set; }
 
+    public int Height { get; set; }
+    public int Length { get; set; }
+    public int Width { get; set; }
+    public int Weight { get; set; }
+
+    public Parcel(int height, int length, int width, int weight)
+    {
+      Height = height;
+      Length = length;
+      Width = width;
+      Weight = weight;
+    }
+
+    public int GetVolume()
+    {
+      return Height * Width * Length;
+    }
+
+    public int GetCostToShip()
+    {
+      return GetVolume() + Weight * 2;
+    }
   }
 }
